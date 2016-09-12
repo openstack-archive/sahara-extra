c808fe5b4b375e4c16fc4e9cb7606280800f41ca
Vitaly Gridnev <vgridnev@mirantis.com>
2016-09-12 12:17:35 +0300

    migrate artifacts to sahara-extra
    Change-Id: I00fc23eba9049002d6498b77636450384ae9dc02

--- tools/build-all-artifacts.sh
+++ tools/build-all-artifacts.sh
@@ -2,10 +2,15 @@
 
 set -eux
 set -o pipefail
+export BRANCH=$1
+export BRANCH=${BRANCH:-master}
 
 echo "Building all needed hadoop-openstack versions"
 hadoop_versions=(2.2.0 2.3.0 2.5.0 2.6.0 2.7.1)
 for ver in ${hadoop_versions[*]}
 do
-    ./tools/build-hadoop-openstack.sh ${ver}
+    ./tools/build-hadoop-openstack.sh $BRANCH ${ver}
 done
+
+cp -r ./common-artifacts ./../dist/
+
